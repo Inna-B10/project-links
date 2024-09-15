b81a835fe658174fcb96dff38ccad503a0725229
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-09-15 23:59:00 +0200

    moved meta tags

--- app/components/ui/MetaTitle.jsx
+++ app/components/ui/MetaTitle.jsx
@@ -3,6 +3,11 @@ import Head from 'next/head'
 const MetaTitle = ({ title }) => {
 	return (
 		<Head>
+			<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+			<meta
+				name='viewport'
+				content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1'
+			/>
 			<title>{title}</title>
 		</Head>
 	)
--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,6 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
-				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-				<meta
-					name='viewport'
-					content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1'
-				/>
 				<body className='antialiased'>
 					<Main />
 					<NextScript />
